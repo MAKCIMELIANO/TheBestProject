dc09d07fd00aec2d52becc9f6b1386dfceac446b
makcimeliano <makcimeliano@gmail.com>
2020-12-23 17:25:26 +0200

    так?

--- mane.py
+++ mane.py
@@ -1,1 +1,12 @@
-print('hello world')
+import requests as req
+from bs4 import BeautifulSoup as BS, BeautifulSoup
+
+resp = req.get("https://www.work.ua/jobs-kyiv-python/")
+
+soup = BeautifulSoup(resp.text, 'lxml')
+
+print(soup.h2)
+print(soup.h2.text)
+print(soup.h2.parent)
+#url = ("https://www.work.ua/jobs-kyiv-python/")
+
